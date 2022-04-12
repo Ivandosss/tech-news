4ea1b189d4a32d8bd2f1aab4afbdec7b525e6988
Pop-Os <ivan@pop-os.localdomain>
2022-04-11 22:52:47 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -24,7 +24,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    button_selector = selector.css("a.tec--btn::attr(href)").get()
+    return button_selector
 
 
 # Requisito 4
