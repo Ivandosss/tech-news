7297deada2e3534c89b59b48fbb47508ae981f96
Pop-Os <ivan@pop-os.localdomain>
2022-04-24 21:48:48 -0300

    Requisito 4, 5, 6, 7, 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,54 @@
+from tech_news.database import db
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = []
+    for new in db.news.find({"title": {"$regex": title, "$options": "i"}}):
+        insert_title = new["title"], new["url"]
+        news.append(insert_title)
+    return news
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_true = ''
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        date_true = True
+    except ValueError:
+        date_true = False
+
+    if date_true:
+        news = []
+        for new in db.news.find(
+            {"timestamp": {"$regex": date, "$options": "i"}}
+        ):
+            insert_title = new["title"], new["url"]
+            news.append(insert_title)
+        return news
+    else:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = []
+    for new in db.news.find(
+            {"sources": {"$elemMatch": {"$regex": source, "$options": "i"}}}
+            ):
+        insert_title = new["title"], new["url"]
+        news.append(insert_title)
+    return news
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = []
+    for new in db.news.find(
+        {"categories": {"$elemMatch": {"$regex": category, "$options": "i"}}}
+            ):
+        insert_title = new["title"], new["url"]
+        news.append(insert_title)
+    return news
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,18 +1,19 @@
 import time
 import requests
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
-def fetch(url, timeout=3):
+def fetch(url):
     try:
-        response = requests.get(url, timeout=timeout)
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
         response.raise_for_status()
-    except (requests.HTTPError, requests.ReadTimeout):
+    except (requests.HTTPError, requests.Timeout):
         return None
-
-    time.sleep(3)
-    return response.text
+    else:
+        return response.text
 
 
 # Requisito 2
@@ -29,11 +30,82 @@ def scrape_next_page_link(html_content):
     return button_selector
 
 
+def scrap(selector, context):
+    return context.css(selector).get()
+
+
+def scrap_all(selector, context):
+    return context.css(selector).getall()
+
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    context = Selector(html_content)
+
+    news = {}
+
+    news["url"] = scrap('head link[rel="canonical"]::attr(href)', context)
+    news["title"] = scrap(".tec--article__header__title::text", context)
+    news["timestamp"] = scrap("#js-article-date::attr(datetime)", context)
+
+    writer = scrap(".z--font-bold *::text", context)
+    news["writer"] = writer.strip() if writer else None
+
+    raw_shares = scrap(".tec--toolbar__item::text", context)
+    if (raw_shares):
+        news["shares_count"] = int(raw_shares.split(" ")[1])
+    else:
+        news["shares_count"] = 0
+
+    raw_comments_count = scrap("#js-comments-btn::attr(data-count)", context)
+    if (raw_comments_count):
+        news["comments_count"] = int(raw_comments_count)
+    else:
+        news["comments_count"] = 0
+
+    summary = scrap_all(
+        "div.tec--article__body > p:nth-child(1) *::text",
+        context
+    )
+    news["summary"] = "".join(summary)
+
+    raw_sources = scrap_all(".z--mb-16 div a::text", context)
+    news["sources"] = [source.strip() for source in raw_sources]
+
+    raw_categories = scrap_all("#js-categories a::text", context)
+    news["categories"] = [category.strip() for category in raw_categories]
+
+    return news
+
+
+def get_all_news(endpoints):
+    all_news = []
+
+    for endpoint in endpoints:
+        all_news.append(scrape_noticia(fetch(endpoint)))
+
+    return all_news
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    page = 1
+    endpoints = []
+
+    while(len(endpoints) < amount):
+        complete_endpoint = "https://www.tecmundo.com.br/novidades"
+        quary = f"?page={page}"
+
+        endpoint = complete_endpoint if page < 2 else complete_endpoint + quary
+
+        for url in scrape_novidades(fetch(endpoint)):
+            if (len(endpoints) < amount):
+                endpoints.append(url)
+
+        page += 1
+
+    all_news = get_all_news(endpoints)
+
+    create_news(all_news)
+
+    return all_news
